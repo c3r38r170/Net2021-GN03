66c6ff43e5db36a29ff18020f5ca058f1271255c
Santiago Abud <c3r38r170@gmail.com>
2021-10-05 00:30:15 +0200

    Quité código obsoleto.

--- Data.Database/UsuarioAdapter.cs
+++ Data.Database/UsuarioAdapter.cs
@@ -6,57 +6,7 @@ using System.Data.SqlClient;
 
 namespace Data.Database {
 	public class UsuarioAdapter : Adapter {
-		#region DatosEnMemoria
-		// Esta región solo se usa en esta etapa donde los datos se mantienen en memoria.
-		// Al modificar este proyecto para que acceda a la base de datos esta será eliminada
-		private static List<Usuario> _Usuarios;
-
-		private static List<Usuario> Usuarios {
-			get {
-				if (_Usuarios == null) {
-					_Usuarios = new List<Business.Entities.Usuario>();
-					Business.Entities.Usuario usr;
-					usr = new Business.Entities.Usuario();
-					usr.ID = 1;
-					usr.State = Business.Entities.BusinessEntity.States.Unmodified;
-					usr.Nombre = "Casimiro";
-					usr.Apellido = "Cegado";
-					usr.NombreUsuario = "casicegado";
-					usr.Clave = "miro";
-					usr.Email = "casimirocegado@gmail.com";
-					usr.Habilitado = true;
-					_Usuarios.Add(usr);
-
-					usr = new Business.Entities.Usuario();
-					usr.ID = 2;
-					usr.State = Business.Entities.BusinessEntity.States.Unmodified;
-					usr.Nombre = "Armando Esteban";
-					usr.Apellido = "Quito";
-					usr.NombreUsuario = "aequito";
-					usr.Clave = "carpintero";
-					usr.Email = "armandoquito@gmail.com";
-					usr.Habilitado = true;
-					_Usuarios.Add(usr);
-
-					usr = new Business.Entities.Usuario();
-					usr.ID = 3;
-					usr.State = Business.Entities.BusinessEntity.States.Unmodified;
-					usr.Nombre = "Alan";
-					usr.Apellido = "Brado";
-					usr.NombreUsuario = "alanbrado";
-					usr.Clave = "abrete sesamo";
-					usr.Email = "alanbrado@gmail.com";
-					usr.Habilitado = true;
-					_Usuarios.Add(usr);
-
-				}
-				return _Usuarios;
-			}
-		}
-		#endregion
-
 		public List<Usuario> GetAll() {
-			//List<Usuario> usuarios = new List<Usuario>(Usuarios);
 			List<Usuario> usuarios = new List<Usuario>();
 			this.OpenConnection();
 			SqlCommand cmdUsuarios = new SqlCommand("SELECT * FROM usuarios",sqlConn);
@@ -75,7 +25,6 @@ namespace Data.Database {
 				}
 			drUsuarios.Close();
 			}catch(Exception Ex){
-				// TODO try catch finally en la donde llamen acá
 				Exception ExcepcionManejada = new Exception("Error al recuperar lista de usuarios", Ex);
 				throw ExcepcionManejada;
 			} finally {
@@ -85,8 +34,6 @@ namespace Data.Database {
 		}
 
 		public Usuario GetOne(int ID) {
-			//return Usuarios.Find(delegate (Usuario u) { return u.ID == ID; });
-
 			Usuario usuario = new Usuario();
 			this.OpenConnection();
 			SqlCommand cmdUsuario = new SqlCommand("SELECT * FROM usuarios WHERE id_usuario=@id", sqlConn);
