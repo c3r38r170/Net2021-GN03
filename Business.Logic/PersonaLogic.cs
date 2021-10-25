19fc09f29e9d4632ec509e4c1bae83bc7fd0748a
Santiago Abud <c3r38r170@gmail.com>
2021-10-25 20:51:43 -0300

    Actualicé nombre de variable.

--- Business.Logic/PersonaLogic.cs
+++ Business.Logic/PersonaLogic.cs
@@ -6,33 +6,26 @@ using System.Threading.Tasks;
 using Business.Entities;
 using Data.Database;
 
-namespace Business.Logic
-{
-    public class PersonaLogic: BusinessLogic
-    {
-        public PersonaAdapter AlumnoData { get; set; }
-        public PersonaLogic()
-        {
-            AlumnoData = new PersonaAdapter();
-        }
-        public List<Persona> GetAll()
-        {
-            return AlumnoData.GetAll();
-        }
+namespace Business.Logic {
+	public class PersonaLogic : BusinessLogic {
+		public PersonaAdapter PersonaData { get; set; }
+		public PersonaLogic() {
+			PersonaData = new PersonaAdapter();
+		}
+		public List<Persona> GetAll() {
+			return PersonaData.GetAll();
+		}
 
-        public void Save(Persona u)
-        {
-            AlumnoData.Save(u);
-        }
+		public void Save(Persona u) {
+			PersonaData.Save(u);
+		}
 
-        public void Delete(int iD)
-        {
-            AlumnoData.Delete(iD);
-        }
+		public void Delete(int iD) {
+			PersonaData.Delete(iD);
+		}
 
-        public Persona GetOne(int iD)
-        {
-            return AlumnoData.GetOne(iD);
-        }
-    }
+		public Persona GetOne(int iD) {
+			return PersonaData.GetOne(iD);
+		}
+	}
 }
