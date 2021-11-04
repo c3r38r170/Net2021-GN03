fa12803453e6d69a86b618c1ab4ba8052e938569
Mark Twian <marcoscalomino@hotmail.com>
2021-11-04 11:14:09 -0300

    form para que el alumno se inscriba a materias

---
+++ UI.Desktop/InscribirseA.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace UI.Desktop
+{
+    partial class InscribirseA
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.dgvCursosInscripcion = new System.Windows.Forms.DataGridView();
+            this.btnInscribirse = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCursosInscripcion)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.btnSalir, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.dgvCursosInscripcion, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnInscribirse, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(722, 424);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 0;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // dgvCursosInscripcion
+            // 
+            this.dgvCursosInscripcion.AllowUserToAddRows = false;
+            this.dgvCursosInscripcion.AllowUserToDeleteRows = false;
+            this.dgvCursosInscripcion.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableLayoutPanel1.SetColumnSpan(this.dgvCursosInscripcion, 2);
+            this.dgvCursosInscripcion.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvCursosInscripcion.Location = new System.Drawing.Point(3, 3);
+            this.dgvCursosInscripcion.Name = "dgvCursosInscripcion";
+            this.dgvCursosInscripcion.ReadOnly = true;
+            this.dgvCursosInscripcion.RowTemplate.Height = 25;
+            this.dgvCursosInscripcion.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvCursosInscripcion.Size = new System.Drawing.Size(794, 415);
+            this.dgvCursosInscripcion.TabIndex = 1;
+            // 
+            // btnInscribirse
+            // 
+            this.btnInscribirse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnInscribirse.Location = new System.Drawing.Point(641, 424);
+            this.btnInscribirse.Name = "btnInscribirse";
+            this.btnInscribirse.Size = new System.Drawing.Size(75, 23);
+            this.btnInscribirse.TabIndex = 2;
+            this.btnInscribirse.Text = "Inscribirse";
+            this.btnInscribirse.UseVisualStyleBackColor = true;
+            this.btnInscribirse.Click += new System.EventHandler(this.btnInscribirse_Click);
+            // 
+            // InscribirseA
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "InscribirseA";
+            this.Text = "InscribirseA";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCursosInscripcion)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.DataGridView dgvCursosInscripcion;
+        private System.Windows.Forms.Button btnInscribirse;
+    }
+}
---
+++ UI.Desktop/InscribirseA.cs
@@ -0,0 +1,74 @@
+﻿using Business.Entities;
+using Business.Logic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI.Desktop
+{
+    public partial class InscribirseA : Form
+    {
+        public CursoLogic CursoLogic { get; set; }
+        public int IdPersonaActual { get; set; }
+        public InscribirseA()
+        {
+            InitializeComponent();
+        }
+        public InscribirseA(int id_persona) : this()
+        {
+            CursoLogic = new CursoLogic();
+            IdPersonaActual = id_persona;
+        }
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnInscribirse_Click(object sender, EventArgs e)
+        {
+            int ID = int.Parse(this.dgvCursosInscripcion.CurrentRow.Cells[0].Value.ToString());
+            Curso c = CursoLogic.GetOne(ID);
+            if (c.Cupo == 0)
+            {
+                MessageBox.Show("NO QUEDAN CUPOS");
+            }
+            else
+            {
+                if (CursoLogic.yaEstaInscripto(ID, IdPersonaActual))
+                {
+                    MessageBox.Show("YA ESTA INSCRIPTO A ESTE CURSO!");
+                }
+                else
+                {
+                    AlumnoInscripcion al = new AlumnoInscripcion();
+                    al.Condicion = "cursando";
+                    al.IDAlumno = IdPersonaActual;
+                    al.IDCurso = c.ID;
+                    al.State = BusinessEntity.States.New;
+                    InscripcionLogic il = new InscripcionLogic();
+                    il.Save(al);
+
+                    c.Cupo = c.Cupo - 1;
+                    c.State = BusinessEntity.States.Modified;
+                    CursoLogic.Save(c);
+
+                    MessageBox.Show("INSCRIPTO CORRECTAMENTE!");
+                }
+
+            }
+
+            this.Listar();
+        }
+
+        public void Listar()
+        {
+            this.dgvCursosInscripcion.DataSource = CursoLogic.GetCursosMateriasComisiones();
+        }
+    }
+}
