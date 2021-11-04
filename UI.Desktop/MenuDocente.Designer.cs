324419efd87e2570786390ff52002bbe036e68b3
Mark Twian <marcoscalomino@hotmail.com>
2021-11-04 09:24:08 -0300

    Menu docente

---
+++ UI.Desktop/MenuDocente.Designer.cs
@@ -0,0 +1,60 @@
+﻿
+namespace UI.Desktop
+{
+    partial class MenuDocente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAgregarNota = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAgregarNota
+            // 
+            this.btnAgregarNota.Location = new System.Drawing.Point(12, 12);
+            this.btnAgregarNota.Name = "btnAgregarNota";
+            this.btnAgregarNota.Size = new System.Drawing.Size(128, 23);
+            this.btnAgregarNota.TabIndex = 0;
+            this.btnAgregarNota.Text = "Agregar Notas";
+            this.btnAgregarNota.UseVisualStyleBackColor = true;
+            // 
+            // MenuDocente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAgregarNota);
+            this.Name = "MenuDocente";
+            this.Text = "MenuDocente";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAgregarNota;
+    }
+}
---
+++ UI.Desktop/MenuDocente.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI.Desktop
+{
+    public partial class MenuDocente : ApplicationForm
+    {
+        public int IdProfesor { get; set; }
+        public MenuDocente()
+        {
+            InitializeComponent();
+        }
+
+        public MenuDocente(int idProfesor) : this()
+        {
+            IdProfesor = idProfesor;
+        }
+
+        private void btnAgregarNota_Click(object sender, EventArgs e)
+        {
+            if (!detectarFormularioAbierto("CargarNota"))
+            {
+                CargarNota cn = new CargarNota(IdProfesor);
+                cn.Show();
+            }
+            else
+            {
+                MessageBox.Show("EL FORMULARIO YA ESTA OPEN");
+            }
+        }
+
+        private bool detectarFormularioAbierto(string formulario)
+        {
+            bool abierto = false;
+
+            if (Application.OpenForms[formulario] != null)
+            {
+                abierto = true;
+            }
+            return abierto;
+        }
+    }
+}
