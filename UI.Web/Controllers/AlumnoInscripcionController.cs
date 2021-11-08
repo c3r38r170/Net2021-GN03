5db92668adcabbca0b4acd0982cd3f8fade1fd85
nachoitur <ignacioiturburu08@gmail.com>
2021-11-08 20:31:55 -0300

    Menú establecido en web

---
+++ UI.Web/Controllers/AlumnoInscripcionController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Entities;
+using Business.Logic;
+
+namespace UI.Web.Controllers
+{
+    public class AlumnoInscripcionController : Controller
+    {
+        // GET: AlumnoInscripcionController/Create
+        public ActionResult InscripcionCreate()
+        {
+            return View(new AlumnoInscripcion());
+        }
+
+        // POST: AlumnoInscripcionController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult InscripcionCreate(AlumnoInscripcion ai)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
