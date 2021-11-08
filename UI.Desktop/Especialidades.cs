1a02aaf5405a29f7678027b9b851e77177652b5f
Mark Twian <marcoscalomino@hotmail.com>
2021-11-08 03:15:44 -0300

    try catch

--- UI.Desktop/Especialidades.cs
+++ UI.Desktop/Especialidades.cs
@@ -54,7 +54,7 @@ namespace UI.Desktop
         private void tsbNuevo_Click(object sender, EventArgs e)
         {
             try
-            {
+            { 
                 EspecialidadDesktop ed = new EspecialidadDesktop(ApplicationForm.ModoForm.Alta);
                 ed.ShowDialog();
                 this.Listar();
