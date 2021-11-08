5033ce62b7f7cfa3c3d06d44c1afb8f08c886ba2
David <dnruiz99@gmail.com>
2021-11-07 23:49:40 -0300

    Abm web Materia

---
+++ UI.Web/Controllers/MateriaController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Entities;
+using Business.Logic;
+
+namespace UI.Web.Controllers
+{
+    public class MateriaController : Controller
+    {
+        
+        [HttpGet]
+        // GET: MateriaController
+        public ActionResult MateriaIndex()
+        {
+            MateriaLogic ml = new MateriaLogic();
+            return View(ml.GetAll());
+        }
+
+        // GET: MateriaController/Create
+        public ActionResult MateriaCreate()
+        {
+            return View(new Materia());
+        }
+
+        // POST: MateriaController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult MateriaCreate(Materia m)
+        {
+            MateriaLogic ml = new MateriaLogic();
+            ml.Save(m);
+            return RedirectToAction("MateriaIndex");
+            /*try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }*/
+        }
+
+        // GET: MateriaController/Edit/5
+        public ActionResult MateriaEdit(int id)
+        {
+            MateriaLogic ml = new MateriaLogic();
+            return View(ml.GetOne(id));
+        }
+
+        // POST: MateriaController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult MateriaEdit(Materia m)
+        {
+            MateriaLogic ml = new MateriaLogic();
+            m.State = BusinessEntity.States.Modified;
+            ml.Save(m);
+            return RedirectToAction("MateriaIndex");
+            /*try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }*/
+        }
+
+        // GET: MateriaController/Delete/5
+        public ActionResult MateriaDelete(int id)
+        {
+            MateriaLogic ml = new MateriaLogic();
+            return View(ml.GetOne(id));
+        }
+
+        // POST: MateriaController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult MateriaDelete(int id, IFormCollection collection)
+        {
+            MateriaLogic ml = new MateriaLogic();
+            ml.Delete(id);
+            return RedirectToAction("MateriaIndex");
+            /*try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }*/
+        }
+    }
+}
