dd73e72d0fb17b04d9d1ab024e87347f3f128901
Santiago Abud <c3r38r170@gmail.com>
2021-11-18 22:28:10 -0300

    Validaciones de edición de usuario.

--- UI.Web/Controllers/UsuarioController.cs
+++ UI.Web/Controllers/UsuarioController.cs
@@ -48,12 +48,31 @@ namespace UI.Web.Controllers
         // POST: UsuarioController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult UsuarioEdit(Usuario usu)
-        {
-            UsuarioLogic ul = new UsuarioLogic();
-            usu.State = BusinessEntity.States.Modified;
-            ul.Save(usu);
-            return RedirectToAction("UsuarioIndex");
+        public ActionResult UsuarioEdit(Usuario usu) {
+			if(!string.IsNullOrWhiteSpace(usu.Nombre)
+				&& !string.IsNullOrWhiteSpace(usu.Apellido)
+				&& !string.IsNullOrWhiteSpace(usu.NombreUsuario)
+				&& !string.IsNullOrWhiteSpace(usu.Clave)
+				&& usu.Clave.Length>=8
+				&& usu.Email != null
+				&& UsuarioLogic.ComprobarFormatoEmail(usu.Email.Trim())) {
+
+				usu.Nombre = usu.Nombre.Trim();
+				usu.Apellido = usu.Apellido.Trim();
+				usu.NombreUsuario = usu.NombreUsuario.Trim();
+				usu.Clave = usu.Clave.Trim();
+				usu.Email = usu.Email.Trim();
+				UsuarioLogic ul = new UsuarioLogic();
+				var usuOriginal = ul.GetOne(usu.ID);
+				usu.ID_Persona = usuOriginal.ID_Persona;
+				usu.PersonaAsociada = usuOriginal.PersonaAsociada;
+				usu.State = BusinessEntity.States.Modified;
+				ul.Save(usu);
+				return RedirectToAction("UsuarioIndex");
+			} else {
+				usu.State = BusinessEntity.States.Unmodified;
+				return View(usu);
+			}
         }
 
         // GET: UsuarioController/Delete/5
