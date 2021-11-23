407eee3035f0beff15686b47df26db078805eac2
dnrz <david_nicolas99@hotmail.com>
2021-11-22 22:19:44 -0300

    Se creo el controlador Registro Notas y la vista que muestra los cursos asignados al docente

---
+++ UI.Web/Controllers/RegistroNotasController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Entities;
+using Business.Logic;
+
+namespace UI.Web.Controllers
+{
+    public class RegistroNotasController : Controller
+    {
+        // GET: RegistroNotasController
+        public ActionResult RegistroNotasIndex()
+        {
+            DocenteCursoLogic dcl = new DocenteCursoLogic();
+            return View(dcl.GetAll());
+        }
+
+        // GET: RegistroNotasController/Details/5
+        public ActionResult Details(int id)
+        {
+           return View();
+        }
+        
+        // GET: RegistroNotasController/Create
+        public ActionResult RegistroNotasCreate(int per)
+        {
+            DocenteCurso dc = new DocenteCurso();
+            dc.IDDocente = per;
+            return View(dc);
+        }
+
+        // POST: RegistroNotasController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RegistroNotasCreate(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RegistroNotasController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: RegistroNotasController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RegistroNotasController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: RegistroNotasController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
